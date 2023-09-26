33e498bfe7a2f8585026a06485016a9410f09319
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-09-26 11:36:47 +0500

    Functions

--- Functions/Functions.js
+++ Functions/Functions.js
@@ -2,143 +2,143 @@
 
 // Write Reusable Code with Function - Funktsiya bilan qayta foydalanish mumkin bo'lgan kodni yozing
 
-// function ourReusableFunction(){
-//     console.log("Hello World");
-// }
-//
-// ourReusableFunction();
-// ourReusableFunction();
+function ourReusableFunction(){
+    console.log("Hello World");
+}
+
+ourReusableFunction();
+ourReusableFunction();
 
 
 
 // Passing Values to Functions with Arguments - Argumentli funksiyalarga qiymatlarni uzatish
 
-// function ourFunctionsWithArg(a, b) {
-//     console.log(a - b);
-// }
-// ourFunctionsWithArg(3, 5)
+function ourFunctionsWithArg(a, b) {
+    console.log(a - b);
+}
+ourFunctionsWithArg(3, 5)
 
 
 
 // Global Scope and Function - Global doirasi va funktsiyasi
 
-// // declare your variable here
-// var myGlobal = 10;
-//
-// function func1() {
-//     // Assign 5 to oopsGlobal here
-//     oopsGlobal = 5;
-// }
-//
-// function func2() {
-//     var output = "";
-//     if (typeof myGlobal != 'undefined'){
-//         output += "myGlobal: " + myGlobal;
-//     }
-//     if (typeof oopsGlobal != "undefined"){
-//         output += "\n oopsGlobal: " + oopsGlobal;
-//     }
-//     console.log(output)
-// }
-// func1();
-// func2();
+// declare your variable here
+var myGlobal = 10;
+
+function func1() {
+    // Assign 5 to oopsGlobal here
+    oopsGlobal = 5;
+}
+
+function func2() {
+    var output = "";
+    if (typeof myGlobal != 'undefined'){
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined"){
+        output += "\n oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output)
+}
+func1();
+func2();
 
 
 
 // Local Scope and Functions - Mahalliy qamrov va funksiyalar
 
-// function myLocalScope() {
-//     var myVar = 5;
-//     console.log(myVar);
-// }
-// myLocalScope()
+function myLocalScope() {
+    var myVar = 5;
+    console.log(myVar);
+}
+myLocalScope()
 
 
 
 
 // Global vs Local Scope in Functions - Funksiyalarda global va mahalliy qamrovi
 
-// var outerWear = "T-Shirt";
-//  function myOutFit() {
-//      var outerWear = "sweater";
-//
-//      return outerWear;
-//  }
-//
-//  console.log(myOutFit());
-//  console.log(outerWear);
+var outerWear = "T-Shirt";
+ function myOutFit() {
+     var outerWear = "sweater";
+
+     return outerWear;
+ }
+
+ console.log(myOutFit());
+ console.log(outerWear);
 
 
 
 // Return a Value from a Function with return - Qaytish bilan funktsiyadan qiymatni qaytaring
 
-// function minusSix(num){
-//     return num - 7;
-// }
-// console.log(minusSix(10))
-//
-//
-// function timeFive(num){
-//     return num * 5;
-// }
-// console.log(timeFive(4))
+function minusSix(num){
+    return num - 7;
+}
+console.log(minusSix(10))
+
+
+function timeFive(num){
+    return num * 5;
+}
+console.log(timeFive(4))
 
 
 
 // Understanding Undefined Value Returned from a Function - Funktsiyadan qaytarilgan aniqlanmagan qiymatni tushunish
 
-// var sum = 0;
-// function addThree() {
-//     sum = sum + 3;
-// }
-//
-// console.log(addThree(4)) // error undefined
-//
-//
-// function addFive(){
-//     sum = sum + 5;
-// }
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+
+console.log(addThree(4)) // error undefined
+
+
+function addFive(){
+    sum = sum + 5;
+}
 
 
 
 
 // Assignment with a Returned Value - Qaytarilgan qiymat bilan tayinlash
 
-// var changed = 0;
-//
-// function change(num){
-//     return (num + 5) / 3;
-//
-// }
-//
-// changed = change(10);
-//
-// console.log(changed)
-//
-// var processed = 0;
-//
-// function processArg(num){
-//     return (num + 3) / 5;
-// }
-//
-// processed = processArg(7);
-//
-// console.log(processed)
+var changed = 0;
+
+function change(num){
+    return (num + 5) / 3;
+
+}
+
+changed = change(10);
+
+console.log(changed)
+
+var processed = 0;
+
+function processArg(num){
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+console.log(processed)
 
 
 
 // Stand in Line - Navbatda turish
 
-// function nextInLine(arr, item) {
-//     arr.push(item)
-//     return arr.shift();
-// }
-//
-// var testArr = [1,2,3,4,5];
-//
-// console.log("Before: " + JSON.stringify(testArr));
-// console.log(nextInLine(testArr, 6));
-// console.log("After: " + JSON.stringify(testArr));
+function nextInLine(arr, item) {
+    arr.push(item)
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
 
 
 
--- Functions/IfStatement.js
+++ Functions/IfStatement.js
@@ -1,172 +1,172 @@
-// if statement
-
-
-// Boolean Values
-function welcomeToBooleans(){
-    return false;
-    //return true;
-}
-
-
-// Use Conditional Logic with If statements
-
-function ourTrueOrFalse(isItTrue){
-    if(isItTrue) {
-        return "Yes, it is True"
-    }
-    return "No, it is False"
-}
-
-// ourTrueOrFalse(true)
-console.log(ourTrueOrFalse(true))
-
-
-
-
-
-// Comparison with the Equality Operator - Tenglik operatori bilan taqqoslash
-
-function testEqual(val){
-    if(val == 12) {
-        return "Yes, it is Equal"
-    }
-    return "No, it is not Equal"
-}
-
-console.log(testEqual(12))
-
-
-
-
-// Comparison with the Strict Equality Operator - Qattiq tenglik operatori bilan taqqoslash
-function testEqual(val){
-    if(val === 12) {
-        return "Yes, it is Equal"
-    }
-    return "No, it is not Equal"
-}
-
-console.log(testEqual(12))
-
-/*
-3 == 3 or '3' it is True
-3 === '3' it is false
-3 === 3 true
- */
-
-
-
-// Practice Comparing Different Values - Turli qiymatlarni solishtirishni mashq qiling
-
-function testEqual(a, b){
-    if(a == b) { // True
-        return "Yes, it is Equal"
-    }
-    return "No, it is not Equal"
-}
-
-console.log(testEqual(10, "10"))
-
+// // if statement
 //
-function testEquals(a, b){
-    if(a === b) { // False
-        return "Yes, it is Equal"
-    }
-    return "No, it is not Equal"
-}
-
-console.log(testEquals(10, "10"))
-
-
-
-
-// Comparison  with the Inequality Operator - Tengsizlik operatori bilan taqqoslash
-
 //
-function testNotEqual(val){
-    if(val != 99) { // False
-        return "No, it is not Equal"
-    }
-    return "Yes, it is Equal"
-}
-
-console.log(testNotEqual(10))
-
-
-
-// Comparison with the Strict Inequality Operator - Qattiq tengsizlik operatori bilan taqqoslash
-
-function testStrictNotEqual(val){
-    if(val !== 17) { // False
-        return "No, it is not Equal"
-    }
-    return "Yes, it is Equal"
-}
-
-console.log(testStrictNotEqual(12))
-
-
-
-// Comparisons with the Logical and Operator - Mantiqiy va operator bilan taqqoslash
-
-
-function testGreaterThan(val){
-    if(val > 100) { // False
-        return "Over 100";
-    }
-    if (val > 10) {
-        return "Over 10";
-    }
-    return "10 or Under"
-}
-
-console.log(testGreaterThan(10))
-
-
-
-// Comparisons with the Greater Than or Equal TO Operator - Operator Katta yoki Teng  bo'lgan bilan taqqoslash
-
-
-function testGreaterOrEqual(val){
-    if(val >= 20) { // False
-        return "20 or Over";
-    }
-    if (val >= 10) {
-        return "10 or Over";
-    }
-    return "Less than 10"
-}
-
-console.log(testGreaterOrEqual(10))
-
-
-
-// Comparison with the Less than Operator - Less than Operator bilan taqqoslash
-
-function testLessThan(val){
-    if(val < 20) { // False
-        return "Under 20";
-    }
-    if (val < 55) {
-        return "Under 55";
-    }
-    return "55 or Over"
-}
-
-console.log(testLessThan(54))
-
-
-
-// Comparison with the Less than or Equal To Operator - Kichik yoki teng operatorlar bilan taqqoslash
-
-function testLessOrEqual(val){
-    if(val <= 12) {
-        return "Smaller than or equal to 12";
-    }
-    if (val <= 24) {
-        return "Smaller than or equal to 24";
-    }
-    return "More than 24"
-}
-
-console.log(testLessOrEqual(54))
+// // Boolean Values
+// function welcomeToBooleans(){
+//     return false;
+//     //return true;
+// }
+//
+//
+// // Use Conditional Logic with If statements
+//
+// function ourTrueOrFalse(isItTrue){
+//     if(isItTrue) {
+//         return "Yes, it is True"
+//     }
+//     return "No, it is False"
+// }
+//
+// // ourTrueOrFalse(true)
+// console.log(ourTrueOrFalse(true))
+//
+//
+//
+//
+//
+// // Comparison with the Equality Operator - Tenglik operatori bilan taqqoslash
+//
+// function testEqual(val){
+//     if(val == 12) {
+//         return "Yes, it is Equal"
+//     }
+//     return "No, it is not Equal"
+// }
+//
+// console.log(testEqual(12))
+//
+//
+//
+//
+// // Comparison with the Strict Equality Operator - Qattiq tenglik operatori bilan taqqoslash
+// function testEqual(val){
+//     if(val === 12) {
+//         return "Yes, it is Equal"
+//     }
+//     return "No, it is not Equal"
+// }
+//
+// console.log(testEqual(12))
+//
+// /*
+// 3 == 3 or '3' it is True
+// 3 === '3' it is false
+// 3 === 3 true
+//  */
+//
+//
+//
+// // Practice Comparing Different Values - Turli qiymatlarni solishtirishni mashq qiling
+//
+// function testEqual(a, b){
+//     if(a == b) { // True
+//         return "Yes, it is Equal"
+//     }
+//     return "No, it is not Equal"
+// }
+//
+// console.log(testEqual(10, "10"))
+//
+// //
+// function testEquals(a, b){
+//     if(a === b) { // False
+//         return "Yes, it is Equal"
+//     }
+//     return "No, it is not Equal"
+// }
+//
+// console.log(testEquals(10, "10"))
+//
+//
+//
+//
+// // Comparison  with the Inequality Operator - Tengsizlik operatori bilan taqqoslash
+//
+// //
+// function testNotEqual(val){
+//     if(val != 99) { // False
+//         return "No, it is not Equal"
+//     }
+//     return "Yes, it is Equal"
+// }
+//
+// console.log(testNotEqual(10))
+//
+//
+//
+// // Comparison with the Strict Inequality Operator - Qattiq tengsizlik operatori bilan taqqoslash
+//
+// function testStrictNotEqual(val){
+//     if(val !== 17) { // False
+//         return "No, it is not Equal"
+//     }
+//     return "Yes, it is Equal"
+// }
+//
+// console.log(testStrictNotEqual(12))
+//
+//
+//
+// // Comparisons with the Logical and Operator - Mantiqiy va operator bilan taqqoslash
+//
+//
+// function testGreaterThan(val){
+//     if(val > 100) { // False
+//         return "Over 100";
+//     }
+//     if (val > 10) {
+//         return "Over 10";
+//     }
+//     return "10 or Under"
+// }
+//
+// console.log(testGreaterThan(10))
+//
+//
+//
+// // Comparisons with the Greater Than or Equal TO Operator - Operator Katta yoki Teng  bo'lgan bilan taqqoslash
+//
+//
+// function testGreaterOrEqual(val){
+//     if(val >= 20) { // False
+//         return "20 or Over";
+//     }
+//     if (val >= 10) {
+//         return "10 or Over";
+//     }
+//     return "Less than 10"
+// }
+//
+// console.log(testGreaterOrEqual(10))
+//
+//
+//
+// // Comparison with the Less than Operator - Less than Operator bilan taqqoslash
+//
+// function testLessThan(val){
+//     if(val < 20) { // False
+//         return "Under 20";
+//     }
+//     if (val < 55) {
+//         return "Under 55";
+//     }
+//     return "55 or Over"
+// }
+//
+// console.log(testLessThan(54))
+//
+//
+//
+// // Comparison with the Less than or Equal To Operator - Kichik yoki teng operatorlar bilan taqqoslash
+//
+// function testLessOrEqual(val){
+//     if(val <= 12) {
+//         return "Smaller than or equal to 12";
+//     }
+//     if (val <= 24) {
+//         return "Smaller than or equal to 24";
+//     }
+//     return "More than 24"
+// }
+//
+// console.log(testLessOrEqual(54))
